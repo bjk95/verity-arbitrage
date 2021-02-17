82e4322ba01fc30eb1bb3e606ed28463bd9b8c3c
bjk95 <bradleykester@quantexa.com>
2021-02-17 19:31:09 +1100

    made changes

--- webapp/ui/src/app/events/event-investigation/event-investigation.component.ts
+++ webapp/ui/src/app/events/event-investigation/event-investigation.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MarketOdds} from "../../models/market-odds";
+import {CompetitorOdds} from "../../models/competitor-odds";
 
 @Component({
   selector: 'app-event-investigation',
@@ -9,18 +10,23 @@ import {MarketOdds} from "../../models/market-odds";
 export class EventInvestigationComponent implements OnInit {
   @Input() market: MarketOdds | undefined;
   @Output() closeInvestigationEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
+  competitorTableColumns = Array( "odds", "site", "margin")
 
   constructor() {
 
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   closeInvestigation(){
     this.closeInvestigationEmitter.emit(false)
     console.log("closing investigation")
+  }
+
+  allOddsThisCompetitor(c: CompetitorOdds): Array<CompetitorOdds>{
+    // @ts-ignore
+    const filtered = this.market.odds.filter((comp: CompetitorOdds) => c.name === comp.name);
+    return filtered.sort((a,b) => a.odds + b.odds )
   }
 
 }
